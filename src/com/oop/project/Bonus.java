379bf839590dd05cbdc6aed72b251946784937ec
WindowsUser <WindowsUser@192.168.1.87>
2022-06-18 07:59:52 +0100

    Enum Pay is now Bonus

---
+++ src/com/oop/project/Bonus.java
@@ -0,0 +1,40 @@
+package com.oop.project;
+
+
+/**
+ * Store the value of the pays multipliers.
+ */
+public enum Bonus {
+	ROYALFLUSH(250),STRAIGHTFLUSH(50),FOURACES(160),FOUR24(80),FOUR5K(50),
+	FULLHOUSE(10),FLUSH(7),STRAIGHT(5),THREEOFAKIND(3),TWOPAIR(1),JACKSORBETTER(1);
+	
+	/**
+	 * Store the values of the plays multipliers 
+	*/
+	private final Integer bonus;
+	
+	/**
+	 * Constructor
+	 * @param pay multiplier value for the game
+	 */
+	Pay(Integer bonus) {
+		this.bonus = bonus;
+	}
+	
+	/**
+	 * Pay variable get function.
+	 * @return pay value
+	 */
+	public Integer getPay() {
+		return bonus;
+	}
+	
+	/**
+	 * String value of pay constants.
+	 * @return String pay value
+	 */
+	@Override
+	public String toString() {
+		return String.valueOf(bonus);
+	}
+}
